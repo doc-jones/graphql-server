52d8ed8c1af8b2e346fb60fcddddf385a38d7551
doc-jones <doc.jones.atwork@gmail.com>
2022-02-12 17:42:39 -0500

    Add graphql endpoint and handler

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -1,8 +1,11 @@
+use std::convert::Infallible;
+
 use async_graphql::Request;
 use serde_json::json;
 
 use crate::schema;
 
+use async_graphql_warp::{Response as GraphQLResponse};
 use async_graphql::{Request, Schema};
 
 use warp::{Filter, Rejection, Reply};
@@ -18,18 +21,20 @@ async fn health_check() -> Result<impl Reply, Rejection> {
 
 pub(super) fn make_routes() -> BoxedFilter<(impl Reply,)> {
 
-    /// Build GraphQL schema.
+    // Build GraphQL schema.
     let schema = schema::build_schema().finish();
     
     
     let health = warp::path::end().and_then(health_check);
 
-    /// GraphQL and subscription handler.
-    let graphql_handler = warp::post().and(warp::path(graphql)).and(
-        async_graphql_warp::graphql(schema).and_then(|(schema, request): (Schema<_, _, _>, Request)| async move {
+    // GraphQL and subscription handler.
+    let graphql_handler = warp::post().and(warp::path(graphql).and(
+            async_graphql_warp::graphql(schema).and_then(|(schema, request): (Schema<_, _, _>, Request)| async move {
+                Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+            }
+        )
+    ));
 
-        }
-    )
 
     health.boxed()
 }
