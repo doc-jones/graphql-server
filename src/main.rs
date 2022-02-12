832e865ea7a349ec4da9b89b97ffba416741a187
doc-jones <doc.jones.atwork@gmail.com>
2022-02-12 05:35:27 -0500

    Add tokio, logging and server::start to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod server;
 
-fn main() {
-    println!("Hello, world!");
+extern crate log;
+extern crate pretty_env_logger;
+
+#[tokio::main]
+async fn main() {
+    // Initialize logger
+    pretty_env_logger::init();
+    
+    // Start server to handle requests.
+    server::start(addr: ([127, 0, 0, 1], 9090)).await;
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,6 +3,6 @@ user std::net::SocketAddr;
 mod routes;
 
 /// Start the API server
-async fn start(addr: impl Into<SocketAddr>) {
+pub async fn start(addr: impl Into<SocketAddr>) {
     warp::serve(routes::make_routes()).run(addr).await;
 }
