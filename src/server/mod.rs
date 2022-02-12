604dc9788ed67c5a0edcab34ad80d4b299900d3e
doc-jones <doc.jones.atwork@gmail.com>
2022-02-12 05:22:57 -0500

    Add server module, connect route for health_check

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,1 +1,8 @@
+user std::net::SocketAddr;
+
 mod routes;
+
+/// Start the API server
+async fn start(addr: impl Into<SocketAddr>) {
+    warp::serve(routes::make_routes()).run(addr).await;
+}
--- src/server/routes.rs
+++ src/server/routes.rs
@@ -1,10 +1,18 @@
 use serde_json::json;
 
-use warp::{Rejection, Reply};
+use warp::{Filter, Rejection, Reply};
+use warp::filters::BoxedFilter;
 use warp::reply::json;
 
 
 // check that the server is alive
 async fn health_check() -> Result<impl Reply, Rejection> {
     Ok(json(&json!("ok": true)))
+}
+
+
+pub(super) fn make_routes() -> BoxedFilter<(impl Reply,)> {
+    let health = warp::path::end().and_then(health);
+
+    health.boxed()
 }
